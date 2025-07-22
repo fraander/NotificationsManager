84c5d3938a5ea2f82236fa4119f0670e402c4181
frank <57777918+fraander@users.noreply.github.com>
2025-07-22 17:56:46 -0400

    Initial Commit

---
+++ Notifications/NotificationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationsApp.swift
+//  Notifications
+//
+//  Created by Frank Anderson on 7/22/25.
+//
+
+import SwiftUI
+
+@main
+struct NotificationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
